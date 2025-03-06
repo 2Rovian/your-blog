5967394a34aaa5ec941368bb81b077e65632a5e4
ian <iangurgel25@hotmail.com>
2025-03-06 18:39:58 -0300

    alternado a navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,27 @@ export default function Navbar() {
         <div className="h-[70px] ">
             <div className="flex justify-between items-center size-full main-div border-b">
                 <span className="text-xl font-semibold"><Link href='/'>YourBlog</Link></span>
-                <div className="flex gap-x-3 text-lg font-medium ">
+
+                {user ?
+                    (<div className="flex gap-x-3 text-lg font-medium">
+                        <span>
+                            <Link href='/'>Home</Link>
+                        </span>
+                        <span>
+                            <Link href={`/user/${user?.username}`}>Profile</Link>
+                        </span>
+                        <span onClick={logout} className="cursor-pointer">
+                            Logout
+                        </span>
+                    </div>
+                    ) :
+                    (<span>
+                        <Link href='/login' className="bg-black text-white dark:bg-white dark:text-black px-2 py-1 rounded-md">Register/Login</Link>
+                    </span>)
+                }
+
+
+                {/* <div className="flex gap-x-3 text-lg font-medium ">
                     <span>
                         <Link href='/'>Home</Link>
                     </span>
@@ -25,7 +45,7 @@ export default function Navbar() {
                         </span>)
                     }
                     
-                </div>
+                </div> */}
             </div>
         </div>
     )

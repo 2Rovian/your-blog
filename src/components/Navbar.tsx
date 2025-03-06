63de0e2c6b852d3023f36f592cb08d029fe6dde2
ian <iangurgel25@hotmail.com>
2025-03-06 20:24:53 -0300

    add um loading

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,37 @@
 'use client'
 import Link from "next/link";
 import { useAuthStore } from "@/store/authStore";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
     const { user, logout } = useAuthStore();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, [user]);
 
     return (
-        <div className="h-[70px] ">
+        <div className="h-[70px]">
             <div className="flex justify-between items-center size-full main-div border-b">
                 <span className="text-xl font-semibold"><Link href='/'>YourBlog</Link></span>
 
-                {user ?
-                    (<div className="flex gap-x-3 text-lg font-medium">
-                        <span>
-                            <Link href='/'>Home</Link>
-                        </span>
-                        <span>
-                            <Link href={`/user/${user?.username}`}>Profile</Link>
-                        </span>
-                        <span onClick={logout} className="cursor-pointer">
-                            Logout
-                        </span>
+                {isLoading ? (
+                    <span>Carregando...</span>
+                ) : user ? (
+                    <div className="flex gap-x-3 text-lg font-medium">
+                        <span><Link href='/'>Home</Link></span>
+                        <span><Link href={`/user/${user.username}`}>Profile</Link></span>
+                        <span onClick={logout} className="cursor-pointer">Logout</span>
                     </div>
-                    ) :
-                    (<span>
-                        <Link href='/login' className="bg-black text-white dark:bg-white dark:text-black px-2 py-1 rounded-md">Register/Login</Link>
-                    </span>)
-                }
-
+                ) : (
+                    <span>
+                        <Link href='/login' className="bg-black text-white dark:bg-white dark:text-black px-2 py-1 rounded-md">
+                            Register/Login
+                        </Link>
+                    </span>
+                )}
             </div>
         </div>
-    )
+    );
 }

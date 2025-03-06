b1b99eaf3286f0f16d32662d408de6c0d0467178
ian <iangurgel25@hotmail.com>
2025-03-06 17:16:22 -0300

    alterando a rota dinamica

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -64,20 +64,34 @@ export default function UserProfilePage() {
           <span className='text-5xl'>{user.username}</span>
           <span>{user.bio || 'Brief Description'}</span>
           <span>Posts Made: {user.postCount || 0}</span>
-          <button className='bg-gray-900 dark:bg-white dark:text-black rounded-sm text-white py-1 my-2 px-6'>
+          <button className='bg-gray-900 dark:bg-white dark:text-black rounded-sm text-white py-1 self-start lg:self-center my-2 px-8'>
             <Link href='create-post'>Create Post</Link>
           </button>
         </div>
       </div>
 
-      <div className="lg:col-span-6 lg:my-5">
+      {/* <div className="lg:col-span-6 lg:my-5">
         {user.posts?.map((post, index) => (
           <div key={index}>
             <h2 className='font-semibold'>{post.title}</h2>
             <h3>{post.content}</h3>
           </div>
         ))}
+      </div> */}
+
+      <div className="lg:col-span-6 lg:my-5">
+        {user.posts?.length == 0 ?
+          <p>Usuário não tem posts feitos.</p>
+          :
+          user.posts?.map((post, index) => (
+            <div key={index}>
+              <h2 className='font-semibold'>{post.title}</h2>
+              <h3>{post.content}</h3>
+            </div>
+          ))
+        }
       </div>
+
     </div>
   )
 }

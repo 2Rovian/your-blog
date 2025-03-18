78432c61ac88c5f41515931c79af591c20ff90de
ian <iangurgel25@hotmail.com>
2025-03-17 21:11:27 -0300

    updates na HomePagem, usando SSR e CSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import News_Selector from "@/components/News_Selector";
+
 interface ArticlesProps {
   author: string,
   title?: string,
-  name?: string,
   description?: string,
   url?: string,
   urlToImage?: string,
   publishedAt?: string,
-  content?: string
 }
 
 interface ResponseProps {
@@ -18,41 +18,17 @@ export default async function Home() {
   const BASE_URL = process.env.API_URL;
 
   const response = await fetch(`${BASE_URL}everything?q=tecnologia&apiKey=${API_KEY}&language=pt&pageSize=20`);
-  const data: ResponseProps = await response.json();
+
+  // const response = await fetch(`${BASE_URL}everything?q=economia OR mercado OR finanças&apiKey=${API_KEY}&language=pt&pageSize=20`);
+
+  // const data: ResponseProps = await response.json();
 
   return (
     <div className="py-5">
-      <p className="mb-5 text-center text-xl font-bold">Principais notícias</p>
-      <div className="flex flex-col md:grid grid-cols-2 md:gap-x-3 gap-y-3">
-        {data.articles?.map((news, index) => (
-          <div key={index} className="bg-gray-100 dark:bg-gray-900 dark:outline dark:outline-1 dark:outline-gray-700 rounded-lg shadow max-h-[400px] px-2 flex justify-between ">
-            <div className="p-2 flex flex-col justify-between">
-              <p className="font-bold">
-                <a href={news.url} target="_blank">
-                  {news.title ?
-                    (news.title.length > 90 ? news.title.slice(0, 100) + "..." : news.title)
-                    : "Título indisponível"}
-                </a>
-              </p>
+      <h1 className="text-start text-2xl font-bold">Ultimas Notícias</h1>
 
-              <div className="flex justify-start gap-x-2">
-                <span className="">{news.author ?
-                  (news.author.length > 20 ? news.author.slice(0, 20) + "..." : news.author)
-                  : "Autor desconhecido"}</span>
-                <span className="">·</span>
-                <span>{news.publishedAt?.split("T")[0]}</span>
-              </div>
+      <News_Selector API_KEY={API_KEY} BASE_URL={BASE_URL} />
 
-            </div>
-            <div className="py-2 flex ">
-              <img src={news.urlToImage ?? '/img_404.png'} alt=""
-                className="object-cover rounded-md max-h-[100px] max-w-[120px] "
-              />
-            </div>
-
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
---
+++ src/components/News_Selector.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+interface News_SelectorProps {
+    API_KEY: string | undefined;
+    BASE_URL: string | undefined;
+}
+
+export default function News_Selector({ API_KEY, BASE_URL }: News_SelectorProps) {
+    const [categoria, setCategoria] = useState<string>('tech');
+    const [articles, setArticles] = useState<any[]>([]);
+
+    const fetchNews = async (category: string) => {
+        let query = '';
+        switch (category) {
+            case 'tech':
+                query = 'tecnologia OR inovação OR IA';
+                break;
+            case 'health':
+                query = 'saúde OR medicina OR bem-estar';
+                break;
+            case 'bussines':
+                query = 'economia OR mercado OR finanças';
+                break;
+            case 'sports':
+                query = 'futebol OR basquete OR olimpíadas';
+                break;
+            case 'entertainment':
+                query = 'filmes OR música OR celebridades';
+                break;
+            default:
+                query = 'tecnologia OR inovação OR IA';
+                break;
+        }
+
+        const response = await fetch(`${BASE_URL}everything?q=${query}&apiKey=${API_KEY}&language=pt&pageSize=20`);
+        const data = await response.json();
+        setArticles(data.articles || []);
+    };
+
+    // Efeito para fazer a requisição sempre que a categoria mudar
+    useEffect(() => {
+        fetchNews(categoria);
+    }, [categoria]);
+
+    return (
+        <div className="mt-1">
+            <ul className="flex items-center gap-x-2 ">
+                <li className={`px-2 py-1 ${categoria == 'tech' ? "outline-2 bg-black text-white dark:bg-white dark:text-black" : "outline-1"} hover:outline-2 duration-500 ease-in-out cursor-pointer outline rounded-sm`}
+                    onClick={() => setCategoria('tech')}
+                >Tecnologia</li>
+                <li className={`px-2 py-1 ${categoria == 'health' ? "outline-2 bg-black text-white dark:bg-white dark:text-black" : "outline-1"} hover:outline-2 duration-500 ease-in-out cursor-pointer outline rounded-sm`}
+                    onClick={() => setCategoria('health')}
+                >Saúde</li>
+                <li className={`px-2 py-1 ${categoria == 'bussines' ? "outline-2 bg-black text-white dark:bg-white dark:text-black" : "outline-1"} hover:outline-2 duration-500 ease-in-out cursor-pointer outline rounded-sm`}
+                    onClick={() => setCategoria('bussines')}
+                >Negócios</li>
+                <li className={`px-2 py-1 ${categoria == 'entertainment' ? "outline-2 bg-black text-white dark:bg-white dark:text-black" : "outline-1"} hover:outline-2 duration-500 ease-in-out cursor-pointer outline rounded-sm`}
+                    onClick={() => setCategoria('entertainment')}
+                >Entretenimento</li>
+                <li className={`px-2 py-1 ${categoria == 'sports' ? "outline-2 bg-black text-white dark:bg-white dark:text-black" : "outline-1"} hover:outline-2 duration-500 ease-in-out cursor-pointer outline rounded-sm hidden sm:block`}
+                    onClick={() => setCategoria('sports')}
+                >Esportes</li>
+            </ul>
+
+            <main className="flex flex-col md:grid grid-cols-2 md:gap-x-3 gap-y-3 mt-5">
+                {articles.map((news, index) => (
+                    <article key={index} className="bg-gray-100 dark:bg-gray-900 dark:outline dark:outline-1 dark:outline-gray-700 rounded-lg shadow max-h-[400px] hover:bg-gray-200 dark:hover:bg-gray-800 cursor-pointer ease-in-out duration-100
+                    ">
+                        <a href={news.url} target="_blank"
+                            className="flex justify-between px-2 overflow-hidden"
+                        >
+                            <div className="p-2 flex flex-col justify-between">
+                                <p className="font-bold">
+                                    {news.title ?
+                                        (news.title.length > 90 ? news.title.slice(0, 100) + "..." : news.title)
+                                        : "Título indisponível"}
+                                </p>
+                                <div className="flex justify-start gap-x-2">
+                                    <span>{news.author || "Autor desconhecido"}</span>
+                                    <span>·</span>
+                                    <span>{news.publishedAt?.split("T")[0]}</span>
+                                </div>
+                            </div>
+                            <div className="py-2 flex">
+                                <img src={news.urlToImage ?? '/img_404.png'} alt=""
+                                    className="object-cover rounded-md max-h-[100px] max-w-[120px]"
+                                />
+                            </div>
+                        </a>
+                    </article>
+                ))}
+            </main>
+        </div>
+    );
+}
